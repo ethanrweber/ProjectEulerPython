9bf8a0532c42c494d2891e0eb8d5a8462ead20c0
ethanrweber <ethanrweber@gmail.com>
2019-11-07 16:26:51 -0600

    add files

---
+++ Problems/1-100/11-20/Problem11.py
@@ -0,0 +1,8 @@
+def method():
+    print("using the given grid, find the largest product of 4 adjacent numbers in the same direction"
+          " (up, down, left, right, or diagonally)")
+
+    filename = "11grid.txt"
+    f = open("test.txt", "r")
+    f.close()
+    return
